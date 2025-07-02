1bc6696cfdb77e441da3f48d38e56600bda50d57
ktvrt <cutbertomo@gmail.com>
2025-07-02 14:26:46 -0600

    creacion de primer componente livewire

---
+++ app/Livewire/BookLista.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class BookLista extends Component
+{
+    public function render()
+    {
+        return view('livewire.book-lista');
+    }
+}
