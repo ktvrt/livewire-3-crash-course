49126d4d807c4d1baa8a9779936ef68fd69556e7
ktvrt <cutbertomo@gmail.com>
2025-07-07 14:13:26 -0600

    validaciones

--- app/Livewire/CreateBook.php
+++ app/Livewire/CreateBook.php
@@ -4,16 +4,24 @@ namespace App\Livewire;
 
 use App\Models\Book;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
 use Livewire\Attributes\Layout;
 
 
 class CreateBook extends Component
 {
+    #[Rule("string|required|min:3|max:50")]
     public $title;
+    #[Rule("string|required|min:3|max:50")]
     public $author;
+    #[Rule("numeric|required|min:1|max:10")]
     public $rating;
 
     public function guardar(){
+        
+        $this->validate();
+
+        // Create a new book record
         Book::create([
             'title' => $this->title,
             'author' => $this->author,
--- resources/views/livewire/create-book.blade.php
+++ resources/views/livewire/create-book.blade.php
@@ -5,17 +5,23 @@
         <div class="field">
             <label for="title">Título:</label>
             <input type="text" id="title" wire:model="title" placeholder="Título del libro">
-
+            @error('title')
+                <span class="error">{{ $message }}</span>
+            @enderror
         </div>
         <div class="field">
             <label for="author">Autor:</label>
             <input type="text" id="author" wire:model="author" placeholder="Autor del libro">
-
+            @error('author')
+                <span class="error">{{ $message }}</span>
+            @enderror
         </div>
         <div class="field">
             <label for="rating">Calificación:</label>
             <input type="text" id="rating" wire:model="rating" placeholder="Calificación del libro">
-
+            @error('rating')
+                <span class="error">{{ $message }}</span>
+            @enderror
         </div>
         <button>Guardar</button>
     </form>
